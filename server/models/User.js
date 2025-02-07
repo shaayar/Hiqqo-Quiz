b3adf56fc4eb4df1b994a732c9078d5579e24016
MansiP10 <mansi.prajapati101998@gmail.com>
2025-02-07 21:56:03 +0530

    forgot password and verify password feature added

--- server/models/User.js
+++ server/models/User.js
@@ -5,6 +5,7 @@ const userSchema = new mongoose.Schema({
   name: { type: String, required: true },
   email: { type: String, required: true, unique: true },
   password: { type: String, required: true },
+  otp: { type: Number, default: 0},
   createdAt: { type: Date, default: Date.now },
 });
 
